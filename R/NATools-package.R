835735031a3d091f758d1cf0b39940a9a3894e93
Ameer Syahir <ameersyahir04@gmailcom>
2024-12-13 16:07:09 +0800

    Initial commit of NATools package

---
+++ R/NATools-package.R
@@ -0,0 +1,12 @@
+#' NATools: A package for handling missing data
+#'
+#' The NATools package provide functions to detect, and handle missing values in the datasets.
+#'
+#' @section Functions:
+#' - `detectNA()` : Identify missing values in a dataset.
+#' - `repNAmed()` : Replace missing values with column median.
+#'
+#' @docType _PACKAGE
+#' @name NATools
+NULL
+
