36a07fc149207615d81aaa9af0191c958ec5f1a5
Ameer Syahir <ameersyahir04@gmailcom>
2024-12-13 15:38:12 +0800

    Initial commit of NATools package

---
+++ R/repNAmed.R
@@ -0,0 +1,26 @@
+#' Replace Missing Values with Medians
+#'
+#' This function replaces missing values in numeric columns with the median of each column.
+#'
+#' @param dataset A data frame or tibble
+#' @return A data frame with missing values replaced by column medians.
+#' @export
+#' @examples
+#' repNAmed(airquality)
+#'
+
+
+repNAmed <- function(dataset) {
+  # Loop through each column
+  for (i in 1:ncol(dataset)) {
+    # Check if the column is numeric and contains missing values
+    if (is.numeric(dataset[[i]]) && any(is.na(dataset[[i]]))) {
+      # Calculate the median for the column, excluding NA values
+      median_value <- median(dataset[[i]], na.rm = TRUE)
+      # Replace missing values with the median
+      dataset[[i]][is.na(dataset[[i]])] <- median_value
+    }
+  }
+  return(dataset)
+}
+
